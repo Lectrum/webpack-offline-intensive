704839dbbe5fe058dea40583a99f9ae1578de8d1
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 19:26:51

--- source/react-components/SkillMeter/index.js
+++ source/react-components/SkillMeter/index.js
@@ -24,7 +24,7 @@ const SkillMeter = hot(
 
             return (
                 <section className = { Styles.skillMeter }>
-                    <h1>My Webpack skill: {skill}</h1>
+                    <h1>My Webpack 11 skill: {skill}</h1>
                     {/* <img src = { cat } /> */}
                     <div>
                         <Button
