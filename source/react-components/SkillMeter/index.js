53b50cf6d6dc5cb463d3f885920668320377c25f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 19:28:59

--- source/react-components/SkillMeter/index.js
+++ source/react-components/SkillMeter/index.js
@@ -1,45 +1,8 @@
 // Core
-import React, { Component } from 'react';
+import React  from 'react';
 import { render } from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-// Styles
-import Styles from './styles.css';
-// import cat from '../../theme/assets/images/cat.jpg';
 
 // Components
-import { Button } from '../Button';
-
-const SkillMeter = hot(
-    class extends Component {
-        state = {
-            skill: 0,
-        };
-
-        inc = () => this.setState(({ skill }) => ({ skill: skill + 1 }));
-        dec = () => this.setState(({ skill }) => ({ skill: skill - 1 }));
-
-        render() {
-            const { skill } = this.state;
-
-            return (
-                <section className = { Styles.skillMeter }>
-                    <h1>My Webpack 11 skill: {skill}</h1>
-                    {/* <img src = { cat } /> */}
-                    <div>
-                        <Button
-                            text = 'Increment'
-                            onClick = { this.inc }
-                        />
-                        <Button
-                            text = 'Decrement'
-                            onClick = { this.dec }
-                        />
-                    </div>
-                </section>
-            );
-        }
-    },
-);
+import { SkillMeter } from './Component';
 
 render(<SkillMeter />, document.getElementById('app'));
