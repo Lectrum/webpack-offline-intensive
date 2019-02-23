34068f537795a0ddf4d79263551527bcf6d45974
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 16:39:46

--- scripts/webpack/config/webpack.dev.js
+++ scripts/webpack/config/webpack.dev.js
@@ -6,11 +6,6 @@ import CleanWebpackPlugin from 'clean-webpack-plugin';
 // Constants
 import { PROJECT_ROOT, SOURCE, BUILD, STATIC } from '../constants';
 
-/**
- * object
- * function
- * promise
- */
 export default () => {
     return {
         mode:    'none',
