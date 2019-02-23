d4b30ea80b56bd62f2f4c70c8bad272d855aa809
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 19:50:25

--- scripts/webpack/modules/javascript.js
+++ scripts/webpack/modules/javascript.js
@@ -3,7 +3,12 @@ export const loadJavaScript = () => ({
         rules: [
             {
                 test: /\.js$/,
-                use:  'babel-loader',
+                use:  {
+                    loader:  'babel-loader',
+                    options: {
+                        compact: false,
+                    },
+                },
             },
         ],
     },
