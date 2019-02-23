a008e61900bcac40d4b12fcd4318813f8c7424c8
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-23 10:57:34 +0200

    → Lesson 1. Checkpoint at: 21:35:02

--- source/react-components/SkillMeter/Component.js
+++ source/react-components/SkillMeter/Component.js
@@ -1,6 +1,12 @@
 // Core
 import React, { Component } from 'react';
+import { setConfig } from 'react-hot-loader';
 import { hot } from 'react-hot-loader/root';
+
+setConfig({
+    ignoreSFC:  true, // RHL will be __completely__ disabled for SFC
+    pureRender: true, // RHL will not change render method
+});
 
 // Styles
 import Styles from './styles.css';
