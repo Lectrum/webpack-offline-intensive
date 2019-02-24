0650c8e3e340c5535222618c377d06c76c9deaf5
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 15:52:46

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -1,3 +1,8 @@
+/**
+ * production — оптимизация включена только в mode: 'production'
+ * development — оптимизация включена только в mode: 'development'
+ * ✓ — оптимизация включена в mode: 'production' и в mode: 'development'
+ */
 export const optimizeBuild = () => ({
 
 })
