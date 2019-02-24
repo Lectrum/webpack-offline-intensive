1f2ec5b99d811d1d014757d8f4d888626517295a
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 10:58:52

--- scripts/webpack/modules/css.js
+++ scripts/webpack/modules/css.js
@@ -3,7 +3,18 @@ export const loadCss = () => ({
         rules: [
             {
                 test: /\.css$/,
-                use:  [ 'style-loader', 'css-loader' ],
+                use:  [
+                    'style-loader',
+                    {
+                        loader:  'css-loader',
+                        options: {
+                            modules:        true,
+                            localIdentName:
+                                '[path][name]__[local]--[hash:base64:5]',
+                            sourceMap: true,
+                        },
+                    },
+                ],
             },
         ],
     },
