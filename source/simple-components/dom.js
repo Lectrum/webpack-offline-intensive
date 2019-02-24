42dd2895718e90d47636e93d5f033fc492bce301
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 17:07:58

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -1,9 +1,18 @@
-export default async (text = '111234 🎉🎉🎉') => {
+export default (text = '111234 🎉🎉🎉') => {
     const element = document.createElement('div');
 
-    const result = await import('./lazyLoadedText');
-
     element.innerHTML = text;
+
+    element.addEventListener('click', async () => {
+        element.textContent = 'preparing...';
+
+        await (() => new Promise((resolve) => setTimeout(resolve, 2000)))();
+        element.textContent = 'fetching...';
+
+        const result = await import('./lazyLoadedText');
+
+        element.textContent = result.default;
+    });
 
     return element;
 };
