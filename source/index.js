5703a40f6220a349f7c119214104f5ada423474f
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 14:39:59

--- source/index.js
+++ source/index.js
@@ -20,29 +20,27 @@
 
 // React
 // import './theme/main.css';
-import './react-components/SkillMeter';
-
-console.log('→ message');
+// import './react-components/SkillMeter';
 
 console.log('→ process.env.NODE_ENV', process.env.NODE_ENV);
 
 // process.env.API_URI
 
-// const middleware = [];
+const middleware = [];
 
-// console.log('→ TWO', TWO);
-// console.log('→ TWO_STRINGIFIED', TWO_STRINGIFIED);
-// console.log('→ TRUE_SIMPLE', TRUE_SIMPLE);
-// console.log('→ __FEATURE__', __FEATURE__);
+console.log('→ TWO', TWO);
+console.log('→ TWO_STRINGIFIED', TWO_STRINGIFIED);
+console.log('→ TRUE_SIMPLE', TRUE_SIMPLE);
+console.log('→ __FEATURE__', __FEATURE__);
 
-// if (process.env.NODE_ENV === 'development') {
-//     middleware.push('redux-logger');
-// }
+if (process.env.NODE_ENV === 'development') {
+    middleware.push('redux-logger');
+}
 
-// if (__DEV__) {
-//     middleware.push('redux-logger');
-// }
+if (__DEV__) {
+    middleware.push('redux-logger');
+}
 
-// if (__FEATURE___) {
-//     middleware.push('redux-logger');
-// }
+if (__FEATURE__) {
+    middleware.push('redux-logger');
+}
