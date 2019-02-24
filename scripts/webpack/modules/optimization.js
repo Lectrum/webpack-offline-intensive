0d4e83a445cbc2912c51b6c41104fd61ef947d13
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 15:53:54

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -1,8 +1,15 @@
+// Core
+import TerserPlugin from 'terser-webpack-plugin';
+
 /**
  * production — оптимизация включена только в mode: 'production'
  * development — оптимизация включена только в mode: 'development'
  * ✓ — оптимизация включена в mode: 'production' и в mode: 'development'
  */
 export const optimizeBuild = () => ({
-
-})
+    optimization: {
+        // production: минификация JavaScript.
+        minimize:  false,
+        minimizer: [ new TerserPlugin() ],
+    },
+});
