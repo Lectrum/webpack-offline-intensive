307bdfbf8413174b9895ec39b48335a85541ffe7
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 13:43:55

--- scripts/webpack/modules/css.js
+++ scripts/webpack/modules/css.js
@@ -1,63 +1,74 @@
 // Core
 import env from 'postcss-preset-env';
 
-export const loadCss = () => ({
+const loadPostCss = (
+    { sourceMap, minimize } = { sourceMap: false, minimize: false },
+) => {
+    const plugins = [
+        env({
+            stage:    0, // default: 2
+            features: {
+                'custom-media-queries': {
+                    importFrom: [
+                        {
+                            customMedia: {
+                                '--phonePortrait':  '(width <= 414px)',
+                                '--phoneLandscape':
+                                    '(width >= 415px) and (width <= 667px)',
+                                '--tabletPortrait':
+                                    '(width >= 668px) and (width <= 768px)',
+                                '--tabletLandscape':
+                                    '(width >= 769px) and (width <= 1024px)',
+                                '--desktopS':
+                                    '(width >= 1025px) and (width <= 1366px)',
+                                '--desktopM':
+                                    '(width >= 1367px) and (width <= 1680px)',
+                                '--desktopL':
+                                    '(width >= 1681px) and (width <= 1920px)',
+                                '--desktopXL': '(width >= 1921px)',
+                            },
+                        },
+                    ],
+                },
+            },
+        }),
+        fontMagician({
+            protocol: 'https:',
+        }),
+    ];
+
+    if (minimize) {
+        plugins.push(cssnano({ preset: [ 'default', { normalizeUrl: false }] }));
+    }
+
+    return {
+        loader:  'postcss-loader',
+        options: {
+            plugins,
+            sourceMap,
+        },
+    };
+};
+
+const loadCss = ({ sourceMap = false } = { sourceMap: false }) => ({
+    loader:  'css-loader',
+    options: {
+        modules:        true,
+        importLoaders:  1,
+        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+        sourceMap,
+    },
+});
+
+export const loadDevCss = () => ({
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use:  [
                     'style-loader',
-                    {
-                        loader:  'css-loader',
-                        options: {
-                            modules:        true,
-                            localIdentName:
-                                '[path][name]__[local]-[hash:base64:5]',
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader:  'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins:   [
-                                // первый плагин
-                                // второй плагин
-                                // ... плагин
-                                env({
-                                    stage:    0, // default: 2
-                                    features: {
-                                        'custom-media-queries': {
-                                            importFrom: [
-                                                {
-                                                    customMedia: {
-                                                        '--phonePortrait':
-                                                            '(width <= 414px)',
-                                                        '--phoneLandscape':
-                                                            '(width >= 415px) and (width <= 667px)',
-                                                        '--tabletPortrait':
-                                                            '(width >= 668px) and (width <= 768px)',
-                                                        '--tabletLandscape':
-                                                            '(width >= 769px) and (width <= 1024px)',
-                                                        '--desktopS':
-                                                            '(width >= 1025px) and (width <= 1366px)',
-                                                        '--desktopM':
-                                                            '(width >= 1367px) and (width <= 1680px)',
-                                                        '--desktopL':
-                                                            '(width >= 1681px) and (width <= 1920px)',
-                                                        '--desktopXL':
-                                                            '(width >= 1921px)',
-                                                    },
-                                                },
-                                            ],
-                                        },
-                                    },
-                                }),
-                                // cssnano — минификатор CSS
-                            ],
-                        },
-                    },
+                    loadCss({ sourceMap: true }),
+                    loadPostCss({ sourceMap: true, minimize: false }),
                 ],
             },
         ],
@@ -69,11 +80,18 @@ export const loadProdCss = () => ({
         rules: [
             {
                 test: /\.css$/,
-                use:  [ 'style-loader', 'css-loader' ],
+                use:  [
+                    MiniCssExtractPlugin.loader,
+                    loadCss({ sourceMap: false }),
+                    loadPostCss({ sourceMap: false, minimize: true }),
+                ],
             },
         ],
     },
     plugins: [
-        /** plugin */
+        new MiniCssExtractPlugin({
+            filename:      'css/[name].css',
+            chunkFilename: 'css/[chunkname].css',
+        }),
     ],
 });
