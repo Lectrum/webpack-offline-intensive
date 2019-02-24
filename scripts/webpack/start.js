7a81746304450c8b21ff5db6c021ef3f323ae444
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-24 10:10:15 +0200

    → Lesson 1. Checkpoint at: 10:33:55

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -39,10 +39,8 @@ const server = new DevServer(compiler, {
     },
 });
 
-server.listen(PORT, HOST, () => {
+server.listen(0, HOST, () => {
     const port = server.listeningApp.address().port;
-    console.log('→ port', port);
-
     const url = `http://${HOST}:${port}`;
     console.log(
         `${chalk.greenBright('→ Server listening on')} ${chalk.blueBright(
@@ -52,3 +50,4 @@ server.listen(PORT, HOST, () => {
 
     openBrowser(url);
 });
+
